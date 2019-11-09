3f891e7c5bd5a843b6a9734d37469f1952d39845
lovf <308409003@qq.com>
2019-11-09 18:36:58 +0800

    5的阶乘

---
+++ njc.c
@@ -0,0 +1,17 @@
+//¼ÆËãn µÄ½×
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <stdlib.h>
+int main()
+{
+int i;
+int sum = 1;
+for (i = 1; i <= 5; i++)
+{
+sum = sum*i;
+}
+printf("%d\n", sum);
+system("pause");
+return 0;
+}
+
