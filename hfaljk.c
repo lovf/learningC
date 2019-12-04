7ff3c71c7e27ff2f2cda81bcc6600e965531e202
lovf <308409003@qq.com>
2019-12-04 19:17:42 +0800

    逻辑操作符，短路求值法，条件操作符，前（后）置++和--

--- hfaljk.c
+++ hfaljk.c
@@ -46,12 +46,12 @@ return 0;
 #include<stdio.h>
 
 int main(){
-	int i = 0, a = 0, b = 1, c = 2, d = 3;
-	//i = a++ or ++b or d++ or c++ or d++;
-	i = a++and ++b and d++and c++and d++;
-	printf(" a=%d\n b=%d\n c=%d\n d=%d\n", a, b, c, d);
-	system("pause");
-	return 0;
+int i = 0, a = 0, b = 1, c = 2, d = 3;
+//i = a++ or ++b or d++ or c++ or d++;
+i = a++and ++b and d++and c++and d++;
+printf(" a=%d\n b=%d\n c=%d\n d=%d\n", a, b, c, d);
+system("pause");
+return 0;
 }
 */
 //条件操作符
@@ -63,20 +63,20 @@ int main(){
 
 例如条件语句：
 
-if(a>b)  max=a;else max=b;可用条件表达式写为 
+if(a>b)  max=a;else max=b;可用条件表达式写为
 
 max=(a>b) ? a : b;
 
 执行该语句的语义是：如a>b为真，则把a赋予max，否则把b 赋予max。*/
 /*#include<stdio.h>
 int Compare(int a, int b){
-	int Max=a > b ? a : b;
-	printf ("%d\n", Max);
+int Max=a > b ? a : b;
+printf ("%d\n", Max);
 }
 int main(){
-	Compare(10, 20);
-	system("pause");
-	return 0;
+Compare(10, 20);
+system("pause");
+return 0;
 }
 */
 //单目操作符
@@ -88,12 +88,12 @@ int main(){
 int main(){
 	int a = 10;
 	int b = 20;
-	int x,y,z,w;
+	int x, y, z, w;
 	//先对a自减，a 的值为9， 然后再使用a导致表达式的值为9。
-	x= --a;
+	x = --a;
 	printf("x=%d a=%d\n", x, a);
 	//先对b自加，b的值为21，然后再使用b导致表达式的值为21。
-    y = ++b;
+	y = ++b;
 	printf("y=%d b=%d\n", y, b);
 	system("pause");
 	return 0;
@@ -102,17 +102,17 @@ int main(){
 //后置--和++
 /*#include<stdio.h>
 int main(){
-	int a = 10;
-	int b = 20;
-	int z, w;
-	//先使用a的值导致表达式的值z=10，然后在对a进行自减a=9；
-	z = a--;
-	printf("z=%d a=%d\n", z, a);
-	//先使用b的值导致表达式的值w=20，然后在对a进行自减b=21；
-	w = b++;
-	printf("w=%d b=%d\n", w, b);
-	system("pause");
-	return 0;
+int a = 10;
+int b = 20;
+int z, w;
+//先使用a的值导致表达式的值z=10，然后在对a进行自减a=9；
+z = a--;
+printf("z=%d a=%d\n", z, a);
+//先使用b的值导致表达式的值w=20，然后在对a进行自减b=21；
+w = b++;
+printf("w=%d b=%d\n", w, b);
+system("pause");
+return 0;
 }
 */
 //一定要分清表达式的值还是自加自减
