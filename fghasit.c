7f333283c9fba089241a028bdec64dc43b9d7153
lovf <308409003@qq.com>
2019-12-18 22:56:33 +0800

    putchar和getchar函数的使用

---
+++ fghasit.c
@@ -0,0 +1,46 @@
+//用字符putchar输出一个字符 BOY//
+//#include <stdio.h>
+//int main()
+//{
+//	//首先对字符进行定义//
+//	char a = 'B', b = 'O', c = 'Y';
+//	putchar(a);
+//	putchar(b);
+//	putchar(c);
+//	putchar('\n');
+//	system("pause");
+//	return 0;
+//}
+//关于getchar函数（字符输入函数）的使用//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include<string.h>
+//int main()
+//{
+//	//首先定义//
+//	char a, b, c;
+//	a = getchar();
+//	b = getchar();
+//	c = getchar();
+//	putchar(a);
+//	putchar(b);
+//	putchar(c);
+//	putchar('\n');
+//	system("pause");
+//	return 0;
+//}
+////定义为字符函数那么输出为putchar函数//
+
+#include <stdlib.h>
+#include<string.h>
+int main()
+{
+	putchar(getchar());
+	putchar(getchar());
+	putchar(getchar());
+	putchar('\n');
+	system("pause");
+	return 0;
+}
+//定义为字符函数那么输出为putchar函数//
+
