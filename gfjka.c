4176f86238a894261b5736e79650e7bbbef70e2a
lovf <308409003@qq.com>
2019-12-25 22:39:36 +0800

    用字ascill实现字符输出

---
+++ gfjka.c
@@ -0,0 +1,15 @@
+//用字ascill实现字符输出//
+#include <stdio.h>
+int main()
+{
+	//首先进行定义//
+	int  a = 65, b = 66, c = 67,d=68,e=69;
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar('\n');
+	system("pause");
+	return 0;
+}
