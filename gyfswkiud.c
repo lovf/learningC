38a55c0c622c866b3ed83e593119d6460b638325
lovf <308409003@qq.com>
2020-07-06 19:18:25 +0800

    520 简单程序

---
+++ gyfswkiud.c
@@ -0,0 +1,35 @@
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int i, k;
+//	printf("ÎÒÎÒÎÒÎÒÎÒÎÒ  °®°®°®°®°®°®  ÄãÄãÄãÄãÄãÄãÄã\n");
+//	for (i = 0; i < 3; i++)
+//		printf("ÎÒ                      °®  Äã          Äã\n");
+//	printf("ÎÒÎÒÎÒÎÒÎÒÎÒ  °®°®°®°®°®°®  Äã          Äã\n");
+//	for (k = 0; k < 3; k++)
+//		printf("          ÎÒ  °®            Äã          Äã\n");
+//	printf("ÎÒÎÒÎÒÎÒÎÒÎÒ  °®°®°®°®°®°®  ÄãÄãÄãÄãÄãÄãÄã\n");
+//	system("pause");
+//	return 0;
+//}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, k;
+	printf("·Æ·Æ·Æ·Æ·Æ·Æ  ·Æ·Æ·Æ·Æ·Æ·Æ   ·Æ·Æ·Æ·Æ·Æ·Æ\n");
+	for (i = 0; i < 3; i++)
+		printf("·Æ                      ·Æ  ·Æ          ·Æ\n");
+	printf("·Æ·Æ·Æ·Æ·Æ·Æ  ·Æ·Æ·Æ·Æ·Æ·Æ  ·Æ          ·Æ\n");
+	for (k = 0; k < 3; k++)
+		printf("          ·Æ  ·Æ            ·Æ          ·Æ\n");
+	printf("·Æ·Æ·Æ·Æ·Æ·Æ   ·Æ·Æ·Æ·Æ·Æ·Æ ·Æ·Æ·Æ·Æ·Æ·Æ·Æ\n");
+	system("pause");
+	return 0;
+}
