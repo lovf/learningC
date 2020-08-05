2510422415e9be6d0b86011e7dbe9887bacaf5b6
lovf <308409003@qq.com>
2020-08-05 10:33:16 +0800

    函数指针的使用和说明

--- fhsdli.c
+++ fhsdli.c
@@ -1,22 +1,15 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//1.判定指针数组和数组指针
-//int* arr[4]: 这是一个数组,数组中的每一个元素都是一个指针变量.
-//int(*arr)[4]: 这是一个指针,这个指针指向的是一个长度为4的int数组.
+//函数指针
 
-#include <stdio.h>
-#include <stdlib.h>
+函数指针，其本质是一个指针变量，该指针指向这个函数。总结来说，函数指针就是指向函数的指针。
+声明格式：类型说明符(*函数名) (参数)
+类型:int(*p)(int x,int y)
+函数指针是需要把一个函数的地址赋值给它，有两种写法：
+fun = &Function；
+fun = Function;
+调用函数指针的方式:
+(*fun)();
+fun();
 
-int main () {
-
-	int arr[4] = { 1, 2, 3, 4 };
-
-	printf("%p\n",arr);
-	printf("%p\n",arr+1);
-	printf("%p\n",&arr+1); 
-
-    system ("pause");
-    return 0;
-}
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -26,14 +19,14 @@ void test() {
 
 }
 
-int main () {
+int main() {
 	void(*func)();  //首先先定义了一个函数指针func,返回值类型为void,
 	func = test; //然后给函数指针赋值,赋值为test,也就是test函数的首地址,此时func就得到了test的地址,所以最终调了func;相当于也就调了test()
 	func();
 
-	
-    system ("pause");
-    return 0;
+
+	system("pause");
+	return 0;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -48,8 +41,8 @@ void test() {
 }
 
 int main() {
-	
-	func  p = test; 
+
+	func  p = test;
 	p();
 	system("pause");
 	return 0;
@@ -59,7 +52,7 @@ int main() {
 #include <stdio.h>
 #include <stdlib.h>
 
-int add_ret(int ,int);
+int add_ret(int, int);
 
 int add(int a, int b, int(*add_value)() { //
 	return (*add_value)(a, b);
@@ -67,7 +60,7 @@ int add(int a, int b, int(*add_value)() { //
 
 int main(void) {
 	int sum = add(3, 4, add_ret);//我们把函数的指针(地址), 这里也就是add_ret, 作为参数int add(int a, int b, int(*add_value)()) ，
-	                             // 这里的参数就是int(*add_value)(), 
+	// 这里的参数就是int(*add_value)(), 
 	printf("sum:%d\n", sum);
 	system("pause");
 	return 0;
@@ -77,37 +70,37 @@ int add_ret(int a, int b) {
 	return a + b;
 }
 
-我们把函数的指针(地址), 这里也就是add_ret, 作为参数int add(int a, int b, int(*add_value)()) ， 
+我们把函数的指针(地址), 这里也就是add_ret, 作为参数int add(int a, int b, int(*add_value)()) ，
 这里的参数就是int(*add_value)(), 这个名字可以随便取，但是要符合C语言的命名规范。当这个指针被用来调用其所指向的函数时，
 我们就说这是回调函数。我们看到add函数内部，return(*add_value)(a, b); 这个(*add_value)(a, b)相当于对指针进行了简引用，
 我们在main函数中，传入具体要实现功能的函数，add_ret, 这个函数很简单，就是实现两数相加并返回, 这里刚刚好，简引用，相当于取出指针返回地址里的值，
 这个值就是return a + b，也就是我们传入a和b两数相加的结果。
 
-函数指针的的两个经典应用场景:
-问题1.模拟实现一个计算器:(if else 和switch 语句)
+//函数指针的的两个经典应用场景 :
+//问题1.模拟实现一个计算器 : (if else 和switch 语句)
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-int Add(int a,int b) {
+int Add(int a, int b) {
 	return a + b;
 }
 
-int Sub(int a,int b) {
+int Sub(int a, int b) {
 	return a - b;
 
 }
 
-int Mul(int a,int b) {
+int Mul(int a, int b) {
 	return a * b;
 
 }
 
-int Div(int a,int b) {
+int Div(int a, int b) {
 	return a / b;
 }
 
-int main () {
+int main() {
 
 	while (1) {
 		int x, y;
@@ -119,16 +112,16 @@ int main () {
 		printf("请输入你的选择: ");
 		scanf("%d", &choice);
 		/*if (choice == 1) {
-			printf("%d\n", Add(20, 10));
+		printf("%d\n", Add(20, 10));
 		}
 		else if (choice == 2) {
-			printf("%d\n", Sub(20, 10));
+		printf("%d\n", Sub(20, 10));
 		}
 		else if (choice == 3) {
-			printf("%d\n", Mul(20, 10));
+		printf("%d\n", Mul(20, 10));
 		}
 		else if (choice == 4) {
-			printf("%d\n", Div(20, 10));
+		printf("%d\n", Div(20, 10));
 		}*/
 		switch (choice) {
 		case 1:
@@ -149,8 +142,8 @@ int main () {
 			break;
 		}
 	}
-    system ("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
 
 
@@ -160,21 +153,21 @@ int main () {
 #include <stdio.h>
 #include <stdlib.h>
 
-int Add(int a,int b) {
+int Add(int a, int b) {
 	return a + b;
 }
 
-int Sub(int a,int b) {
+int Sub(int a, int b) {
 	return a - b;
 
 }
 
-int Mul(int a,int b) {
+int Mul(int a, int b) {
 	return a * b;
 
 }
 
-int Div(int a,int b) {
+int Div(int a, int b) {
 	return a / b;
 }
 
@@ -183,8 +176,8 @@ void Print() {
 	printf("选择错误\n");
 }
 
-int main () {
-	func table[] = { Print ,Add,Sub,Mul,Div};
+int main() {
+	func table[] = { Print, Add, Sub, Mul, Div };
 	int choice = 0;
 	while (1) {
 		printf("0.退出计算器\n");
@@ -198,12 +191,12 @@ int main () {
 		int ret = table[choice](20, 10);
 		printf("%d\n", ret);
 	}
-    system ("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+//
 //问题2:冒泡排序
 #define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -226,7 +219,7 @@ int main() {
 	int choice = 0;
 	int arr[4] = { 9, 5, 2, 7 };
 	int size = sizeof(arr) / sizeof(arr[0]);
-	
+
 	BubbleSort(arr, size);
 	for (int i = 0; i < size; i++) {
 		printf("%d ", arr[i]);
@@ -253,17 +246,17 @@ void BubbleSort(int arr[], int size, int Asc) {
 					arr[cur] = temp;
 				}
 			}
-				else if (Asc == 0) { 
-					if (arr[cur - 1] < arr[cur]) {
-						int temp = arr[cur - 1];
-						arr[cur - 1] = arr[cur];
-						arr[cur] = temp;
-					}
+			else if (Asc == 0) {
+				if (arr[cur - 1] < arr[cur]) {
+					int temp = arr[cur - 1];
+					arr[cur - 1] = arr[cur];
+					arr[cur] = temp;
 				}
-
 			}
+
 		}
 	}
+}
 
 int main(){
 	int arr[] = { 9, 5, 2, 7 };
@@ -280,7 +273,7 @@ int main(){
 	return 0;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-函数指针实现
+//函数指针实现
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -319,184 +312,3 @@ int main(){
 	system("pause");
 	return 0;
 }
-//结合指针 ,数组 常考字符长度字符串长度的举例
-#include <stdio.h>
-#include <stdlib.h>
-
-int main () {
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	第一组
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	int arr[4] = { 1, 2, 3, 4 };
-	printf("%d\n",sizeof(arr)); // 16 sizeof 求得是字符数组所占字节的大小.
-	printf("%d\n", sizeof(arr+1));// 4 数组名参与运算会隐式转为指针,指向首元素的地址(int*) 指针在32位操作系统下占4个字节,在64位操作系统下占8个字节
-	printf("%d\n", sizeof(arr+0));//4 同上 数组名参与运算会隐式转为指针
-	printf("%d\n", sizeof(*arr)); //4 arr等于已经给隐式转为指针,指向了首元素的地址,然后再解引用(*arr),就得到了整数1,一个int在32位操作系统用下占4个字节
-	printf("%d\n", sizeof(arr[1])); // 4 指向整型元素2
-	printf("%d\n", sizeof(&arr)); // 4 &arr得到一个数组指针,int(*)[],数组指针也是一个指针指向一个长度为4的整型数组.
-	printf("%d\n", sizeof(&arr+1)); //4 同上仍然是一个数组指针,
-	printf("%d\n", sizeof(&arr[0])); //4 int* 
-	printf("%d\n", sizeof(&arr[0]+1));// 4 同上
-	 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	第二组
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
-	printf("%d\n",sizeof(arr)); // 6 求字符数组所占的字节大小,字符数组最后没有\0因此在计算的时候不用求\0的长度
-	printf("%d\n", sizeof(arr+0)); // 4 会隐式转化为指针指向首元素的地址
-	printf("%d\n", sizeof(*arr));//arr直接打印会隐式转为指针,指向首元素的地址,然后在进行解引用得到一个字符'a',一个字符在32位操作系统下占一个字节
-	printf("%d\n", sizeof(arr[1]));//根据下标直接指向的是字符'b'
-	printf("%d\n", sizeof(&arr));// 4 &arr得到一个数组指针,数组指针仍然是指针载2位操作系统下占4个字节的大小
-	printf("%d\n", sizeof(&arr)); //4  数组指针+1仍然是数组指针 ,指针占4个字节
-	printf("%d\n", sizeof(&arr[0]+1));//&arr[0]得到的是一个char* ,char*+1仍然是一个char*类型,占4个字节的大小
-
-    system ("pause");
-    return 0;
-}
-///
-总结:c语言中关于字符串和字符串数组的区别:
-1.字符串是字符数组的特殊情况 字符串是以\0为结尾的 
-strlen函数求得是字符串的长度(即找到\0)
-sizeof求得是所占的字节大小
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
